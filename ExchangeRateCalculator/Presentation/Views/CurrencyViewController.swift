993b1b086d6227193236f32e363cc12eff1a16f1
mephrine <dthwrld@gmail.com>
2022-03-02 00:30:52 +0900

    feat($viewcontroller): add binding view model and view

--- ExchangeRateCalculator/Presentation/Views/CurrencyViewController.swift
+++ ExchangeRateCalculator/Presentation/Views/CurrencyViewController.swift
@@ -8,18 +8,32 @@
 import UIKit
 
 class CurrencyViewController: UIViewController {
+	private enum UI {
+		static let remittanceTextFieldMaxLength: Int = 5
+	}
+	
 	// MARK: - Properties
 	private let viewModel: CurrencyViewModel
+	private let pickerItems = ReceiptCountry.allCases
 	
 	// MARK: - Views
 	private var currencyView: CurrencyView {
 		self.view as! CurrencyView
 	}
 	
+	private lazy var countryPickerView: UIPickerView = {
+		let pickerView = UIPickerView()
+		self.view.addSubview(pickerView)
+		pickerView.delegate = self
+		pickerView.dataSource  = self
+		return pickerView
+	}()
+	
 	// MARK: - Initialize
 	init(viewModel: CurrencyViewModel) {
 		self.viewModel = viewModel
-		super.init()
+		super.init(nibName: nil, bundle: nil)
+		viewModel.delegate = self
 	}
 	
 	required init?(coder: NSCoder) {
@@ -29,25 +43,83 @@ class CurrencyViewController: UIViewController {
 	// MARK: - LifeCycle
 	override func loadView() {
 		self.view = CurrencyView()
-		setupUI()
-		initUI()
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setupUI()
+		viewModel.loadedView()
 	}
 }
 
 fileprivate extension CurrencyViewController {
 	func setupUI() {
 		currencyView.remittanceTextField.delegate = self
-	}
-	
-	func initUI() {
-		
+		currencyView.remittanceTextField.addTarget(self, action: #selector(didChangeTextField(textField:)), for: .editingChanged)
 	}
 }
 
-extension CurrencyViewController: UITextFieldDelegate {
+// MARK: - Actions
+extension  CurrencyViewController {
+	@objc func didChangeTextField(textField: UITextField) {
+		guard let changedText = textField.text else { return }
+		viewModel.changedRemittanceTextField(to: changedText)
+	}
+}
+
+// MARK: - CurrencyViewModelDelegate
+extension CurrencyViewController: CurrencyViewModelDelegate {
+	func currencyViewModel(_ currencyViewModel: CurrencyViewModel, didChangeCurrency currency: Currency, of country: ReceiptCountry) {
+		currencyView.changeCurrency(by: currency, and: country)
+	}
 	
+	func currencyViewModel(_ currencyViewModel: CurrencyViewModel, didChangeAmountReceived amountReceived: AmountReceived) {
+		currencyView.changeAmountReceived(by: amountReceived)
+	}
+	
+	func currencyViewModel(_ currencyViewModel: CurrencyViewModel, didOccurServerError error: ServerError) {
+		currencyView.showErrorMessage(of: error)
+	}
+	
+	func currencyViewModel(_ currencyViewModel: CurrencyViewModel, didOccurRemittanceValueError error: Remittance.ValueError) {
+		currencyView.showErrorMessage(of: error)
+	}
+	
+	func currencyViewModel(_ currencyViewModel: CurrencyViewModel, didOccurExchangeRateCalculatorValueError error: ExchangeRateCalculator.ValueError) {
+		currencyView.showErrorMessage(of: error)
+	}
+}
+
+// MARK: - UITextFieldDelegate
+extension CurrencyViewController: UITextFieldDelegate {
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		let oldText = textField.text ?? ""
+		guard let oldTextRange = Range(range, in: oldText) else { return false }
+		
+		let newText = oldText.replacingCharacters(in: oldTextRange, with: string)
+		return newText.count <= UI.remittanceTextFieldMaxLength
+	}
+}
+
+// MARK: - UIPickerViewDataSource
+extension CurrencyViewController: UIPickerViewDataSource {
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return pickerItems.count
+	}
+}
+
+
+// MARK: - UIPickerViewDelegate
+extension CurrencyViewController: UIPickerViewDelegate {
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		pickerItems[row].countryUnit
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		viewModel.selectedReceiptCountryPickerItem(pickerItems[row])
+	}
 }
